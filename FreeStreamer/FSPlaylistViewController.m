f0d96e9dd03501414be2590941e8030020bdb59c
iPonyRadio <pixelperfectpon3@gmail.com>
2013-09-25 09:24:06 -0400

    Update FSPlaylistViewController.m
    Switch XML URL to the iPonyRadio station list

--- FreeStreamer/FSPlaylistViewController.m
+++ FreeStreamer/FSPlaylistViewController.m
@@ -34,7 +34,7 @@
     __weak FSPlaylistViewController *weakSelf = self;
     
     _request = [[FSParsePlaylistFeedRequest alloc] init];
-    _request.url = @"http://muhonen.net/project/FreeStreamer/example-rss-feed.xml";
+    _request.url = @"http://iPonyRadio.tk/ststions.xml";
     _request.onCompletion = ^() {
         [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
         
